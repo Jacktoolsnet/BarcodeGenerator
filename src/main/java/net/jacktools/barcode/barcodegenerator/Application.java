385a05b4e4eb736d50b4b6ede74478b2a123c8a8
Christian Wagner <jacktoolsnet@gmail.com>
2022-12-30 14:06:58 +0100

    Add Assets class

--- src/main/java/net/jacktools/barcode/barcodegenerator/Application.java
+++ src/main/java/net/jacktools/barcode/barcodegenerator/Application.java
@@ -3,12 +3,14 @@ package net.jacktools.barcode.barcodegenerator;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.jacktools.barcode.barcodegenerator.utils.Assets;
 
 import java.io.IOException;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class Application extends javafx.application.Application {
+
+    private static Assets ASSETS = new Assets();
+
     public static void main(String[] args) {
         launch();
     }
@@ -16,7 +18,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("mainView.fxml"));
-        fxmlLoader.setResources(ResourceBundle.getBundle("i18n", Locale.getDefault()));
+        fxmlLoader.setResources(Assets.RESOURCEBUNDLE);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Barcode generator");
         stage.setScene(scene);
