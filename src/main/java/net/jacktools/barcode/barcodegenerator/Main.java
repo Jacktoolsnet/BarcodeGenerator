08063197c1013000749039d273f74e09ebbcd1dc
Christian Wagner <jacktoolsnet@gmail.com>
2024-03-31 15:47:59 +0200

    MSI-Package

---
+++ src/main/java/net/jacktools/barcode/barcodegenerator/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024.
+ */
+
+package net.jacktools.barcode.barcodegenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+
+}
